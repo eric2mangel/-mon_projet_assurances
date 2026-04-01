df6b4a42959412446c4f4a356768795e88c6256f
Eric Demangel <eric2mangel@gmail.com>
2026-04-01 08:34:44 +0200

    Ajoute calcul prime moyenne par région

--- analyse.py
+++ analyse.py
@@ -1,8 +1,13 @@
+
 import pandas as pd
 
-# Chargement des donnÃ©es
+# Chargement des données
 df = pd.read_parquet('../data/assurances_enrichi.parquet')
 
-# Calcul de la prime moyenne
+# Calcul de la prime moyenne globale
 prime_moy = df['Prime_estimee'].mean()
-print(f"Prime moyenne : {prime_moy:.2f} â‚¬")
+print(f"Prime moyenne : {prime_moy:.2f} €")
+
+# Calcul par région (ajout)
+prime_region = df.groupby('Region_MAJ')['Prime_estimee'].mean().sort_values(ascending=False)
+print(prime_region)
